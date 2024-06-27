788cb3471d8d6cb8939bab3984a1f563fecb8f2d
RodrigoFernandes <rodrigohf79@hotmail.com>
2024-06-26 21:44:25 -0300

    initial Project

---
+++ src/test/java/br/com/example/challenge/screenMusic/ScreenMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.challenge.screenMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
